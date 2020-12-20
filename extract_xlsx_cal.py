010efc81de7fbd96102936759afcb71790e0f7db
Stephan Wiesenhütter <swiesenhuetter@amberg.ch>
2020-12-20 14:26:10 +0100

    create ical format strings from day and time

--- extract_xlsx_cal.py
+++ extract_xlsx_cal.py
@@ -1,19 +1,35 @@
 from openpyxl import load_workbook
+from icalendar import Calendar, Event
 import datetime
 import re
 
-wb = load_workbook(filename = '201214_PSUE_Schiesskalender2021.xlsx')
-events = wb['Hauptplan2021']
-shtime = events['C']
-for t in shtime:
-    match = re.findall(r'\d{4}-\d{4}',t.value)
-    if match:
-        day = events.cell(row=t.row, column=1).value
-        print(day.strftime("%b %d %Y "), end='')
-        [print (' {} '.format(x), end='') for x in match]
-        start_time = match[0][0:4]
-        datetime.datetime.strptime(start_time,'%H%M')
-        end_time = match[-1][5:9]
-        datetime.datetime.strptime(end_time,'%H%M')
-        print()
 
+def event_datetime(day, hours_list):
+    if not isinstance(day, datetime.datetime):
+        print ('{} {}'.format(day, hours_list ))
+        return
+    start_txt = hours_list[0][0:4]
+    end_txt = hours_list[-1][5:9]
+    day_str = datetime.datetime.strftime(day, '%Y%m%d')
+    start_ical_fmt = "{}T{}00".format(day_str, start_txt)
+    end_ical_fmt = "{}T{}00".format(day_str, end_txt)
+    return start_ical_fmt, end_ical_fmt
+
+def xl_to_calendar(xslfile):
+    wb = load_workbook(filename = xslfile)
+    xsl_cal = wb['Hauptplan2021']
+    shtime = xsl_cal['C']
+    for t in shtime:
+        match = re.findall(r'\d{4}-\d{4}',t.value)
+        if match:
+            day = xsl_cal.cell(row=t.row, column=1).value
+            event_datetime(day, match)
+            print()
+
+
+def main():
+    xl_to_calendar('201214_PSUE_Schiesskalender2021.xlsx')
+
+
+if __name__ == "__main__":
+    main()
--- test_shootical.py
+++ test_shootical.py
@@ -1,1 +1,11 @@
 import pytest
+import extract_xlsx_cal
+from datetime import datetime
+
+
+def test_event_datetime():
+    dtpast = datetime.strptime("19780311T121314","%Y%m%dT%H%M%S")
+    assert dtpast.year == 1978
+    evt_start, evt_end = extract_xlsx_cal.event_datetime(dtpast, ["1200-1400"])
+    assert len(evt_start) == 15
+    assert len(evt_end) == 15
