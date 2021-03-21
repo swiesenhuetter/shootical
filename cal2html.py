4e5e70cb1cba5c2a6a5a914f7aeff952abad242e
Stephan Wiesenhütter <stephan.wiesenhuetter@eulitha.com>
2021-03-21 21:23:20 +0100

    last changed date

--- cal2html.py
+++ cal2html.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from jinja2 import Template 
 import extract_xlsx_events as extr
 import urllib.request as urq
@@ -18,16 +19,16 @@ template = ""
 with open('psue_cal.html.jinja','r') as file: 
     template = file.read()
 
-
-event_data = {'events' : extr.xl_to_events(local_name)}
+event_data = {'events' : extr.xl_to_events(local_name),
+              'date' : datetime.now()}
 
 j2_template = Template(template)
 j2_template.globals['odd_month'] = extr.odd_month
 
-html = j2_template.render(event_data)
+html = j2_template.render(event_data).encode('ascii', 'xmlcharrefreplace')
 
 
 with open('psue_cal.html','w') as file: 
-    template = file.write(html)
+    template = file.write(html.decode())
 
 
